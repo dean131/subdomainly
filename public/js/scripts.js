6fbf4c15a310f341f227111e22508f332583fdae
dean131 <deanabdillah@gmail.com>
2024-09-08 07:15:55 +0700

    Refactor showAlert function to display error message in a styled alert

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,9 @@
-function showAlert(message) {}
+function showAlert(message) {
+    const alert = document.getElementById("alert");
+    alert.innerHTML = /*html*/ `
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        ${message}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    `;
+}
